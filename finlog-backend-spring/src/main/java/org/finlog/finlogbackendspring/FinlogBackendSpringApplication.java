ddb7a5f1427e370454df599d207db00cb67deb78
Henrique Lopes Bonfanti <henriquebonfanti2018@gmail.com>
2025-06-03 15:52:04 -0300

    chore: initial commit

---
+++ finlog-backend-spring/src/main/java/org/finlog/finlogbackendspring/FinlogBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package org.finlog.finlogbackendspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinlogBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinlogBackendSpringApplication.class, args);
+    }
+
+}
