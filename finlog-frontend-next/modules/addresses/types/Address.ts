4ca054ba5ed4d3df14541cc3c87ffeeee5e52858
Henrique Lopes Bonfanti <henriquebonfanti2018@gmail.com>
2025-06-08 12:11:46 -0300

    feat(4): add ExpensesApiSlice for expense requests

---
+++ finlog-frontend-next/modules/addresses/types/Address.ts
@@ -0,0 +1,10 @@
+export interface Address {
+  street: string
+  streetNumber: string
+  city: string
+  state: string
+  zipCode: string
+  country: string
+  neighborhood: string
+  complement?: string
+}
---
+++ finlog-frontend-next/modules/expenses/lib/redux/ExpensesApiSlice.ts
@@ -0,0 +1,23 @@
+import { apiSlice } from '@/common/lib/redux/apiSlice'
+import { Expense, ExpenseList } from '../../types/Expense'
+import { SuccessResponse } from '@/common/types/api/ApiResponse'
+import { ExpensesMapper } from '../../mappers/ExpensesMapper'
+
+const expensesMapper = new ExpensesMapper()
+
+const expensesApiSlice = apiSlice.injectEndpoints({
+  overrideExisting: true,
+  endpoints: (builder) => ({
+    getExpenses: builder.query<ExpenseList[], void>({
+      query: () => ({
+        url: '/despesas',
+        method: 'GET',
+      }),
+      transformResponse: (response: SuccessResponse<Expense[]>) =>
+        response.data.map((expense) => expensesMapper.map(expense)),
+      transformErrorResponse: () => null,
+    }),
+  }),
+})
+
+export const { useGetExpensesQuery } = expensesApiSlice
