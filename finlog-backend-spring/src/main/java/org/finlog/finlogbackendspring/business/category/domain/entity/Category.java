17e87eb5a3a13a9489de8674402a9768d84939ac
Henrique Lopes Bonfanti <henriquebonfanti2018@gmail.com>
2025-06-07 21:44:40 -0300

    feat(4): add Category entity and corresponding tests

---
+++ finlog-backend-spring/src/main/java/org/finlog/finlogbackendspring/business/category/domain/entity/Category.java
@@ -0,0 +1,34 @@
+package org.finlog.finlogbackendspring.business.category.domain.entity;
+
+import org.finlog.finlogbackendspring.config.entity.DomainEntity;
+
+public class Category extends DomainEntity {
+
+    private String name;
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(long id, String name, String description) {
+        super(id);
+        this.setName(name);
+        this.setDescription(description);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
