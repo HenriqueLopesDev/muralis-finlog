99ff6eea97580ab6df3e03892317959ae615d644
Henrique Lopes Bonfanti <henriquebonfanti2018@gmail.com>
2025-06-09 15:54:01 -0300

    feat(5): add ExpenseMapper implementation and define UseCase interface

--- finlog-backend-spring/src/main/java/org/finlog/finlogbackendspring/business/expense/domain/mapper/ExpenseToExpenseList.java
+++ finlog-backend-spring/src/main/java/org/finlog/finlogbackendspring/business/expense/domain/mapper/ExpenseToExpenseList.java
@@ -3,12 +3,12 @@ package org.finlog.finlogbackendspring.business.expense.domain.mapper;
 import org.finlog.finlogbackendspring.business.category.infrastructure.dto.CategorySummaryDto;
 import org.finlog.finlogbackendspring.business.expense.domain.entity.Expense;
 import org.finlog.finlogbackendspring.business.expense.infrastructure.http.dto.ExpenseListDto;
-import org.finlog.finlogbackendspring.config.mapper.AbstractMapper;
+import org.finlog.finlogbackendspring.config.mapper.Mapper;
 
-public class ExpenseMapper extends AbstractMapper<Expense, ExpenseListDto> {
+public class ExpenseMapper implements Mapper<Expense, ExpenseListDto> {
 
     @Override
-    public ExpenseListDto toDto(Expense expense) {
+    public ExpenseListDto map(Expense expense) {
         return new ExpenseListDto(
             expense.getId(),
             expense.getDescription(),
