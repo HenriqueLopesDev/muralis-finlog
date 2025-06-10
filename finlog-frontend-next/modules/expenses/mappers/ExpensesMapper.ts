7e465e68aeecdc894b2602956864066993639939
Henrique Lopes Bonfanti <henriquebonfanti2018@gmail.com>
2025-06-10 02:09:56 -0300

    feat(11): update expenses mapping to support pagination

--- finlog-frontend-next/modules/expenses/mappers/ExpensesMapper.ts
+++ finlog-frontend-next/modules/expenses/mappers/ExpensesMapper.ts
@@ -1,19 +1,27 @@
 import { IMapper } from '@/common/types/IMapper'
 import { Expense, ExpenseList } from '../types/Expense'
+import { PaginatedData, PaginatedResponse } from '@/common/types/api/ApiResponse'
 
-export class ExpensesMapper implements IMapper<Expense, ExpenseList> {
-  map(baseType: Expense): ExpenseList {
+export class ExpensesMapper implements IMapper<PaginatedResponse<Expense>, PaginatedData<ExpenseList>> {
+  map(baseType: PaginatedResponse<Expense>): PaginatedData<ExpenseList> {
     return {
-      id: baseType.id,
-      value: baseType.value,
-      description: baseType.description,
-      date: baseType.date,
+      content: baseType.data.content.map((expense) => this.mapExpense(expense)),
+      pagination: baseType.data.pagination
+    }
+  }
+  
+  private mapExpense(expense: Expense): ExpenseList {
+    return {
+      id: expense.id,
+      value: expense.value,
+      description: expense.description,
+      date: expense.date,
       category: {
-        id: baseType.category.id,
-        name: baseType.category.name,
+        id: expense.category.id,
+        name: expense.category.name,
       },
-      paymentType: baseType.paymentType,
-      address: baseType.address,
+      paymentType: expense.paymentType,
+      address: expense.address,
     }
   }
 }
